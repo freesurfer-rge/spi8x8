d9b42dad33d7103c499ca6714595bce6a6fd3a6b
R Edgar <freesurfer.rge@gmail.com>
2021-04-02 09:17:46 -0400

    Sort out pigpiod

--- main.cpp
+++ main.cpp
@@ -6,5 +6,17 @@
 int main() {
   std::cout << "LED 8x8 SPI Driver" << std::endl;
 
+  const int piId = pigpio_start(nullptr, nullptr);
+  if( piId< 0 ) {
+    std::cerr << "Could not connect to pigpiod."
+	      << "Have you run 'sudo pigpiod' ?"
+	      << std::endl;
+    return(-1);
+  }
+
+
+
+  pigpio_stop(piId);
+  
   return 0;
 }
